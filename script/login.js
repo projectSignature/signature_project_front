2709a3cb8495858959010e9d91d1c31f601d1595
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-10-24 08:05:47 +0900

    finalização da página de login

---
+++ script/login.js
@@ -0,0 +1,95 @@
+
+let accessmainserver = 'http://localhost:3333'　　//メインサーバーのチェックアクセス先
+let machines = []
+let user
+let password
+let errormessage
+document.getElementById("login-bottom").addEventListener("click", login_check)
+
+
+//ログイン情報の確認をする処理、IDが空白かどうか、その後PASSがくうはくかどうか、TRUEの場合Swal処理
+function login_check(user,password){
+  //verificar campos vazios
+   user = document.getElementById("user").value;
+   password = document.getElementById("pass").value;
+  if(user == ""){
+    errormessage = "ユーザー名を入力してください"
+    swallopen(errormessage)
+  }else{
+    if(password == ""){
+      errormessage = "パスワードを入力してください"
+      swallopen(errormessage)
+    }else{
+      login_request(user,password) //chamar função de login passando login e senha
+    }
+  }
+  console.log('mondai nasji')
+}
+//validar dados
+async function login_request(user,password){
+  await axios.post(accessmainserver+'/auth',{
+      numbers:user,
+      password:password })
+   .then((response) => {
+    
+    if(response.data.token){
+      sessionStorage.setItem("token",response.data.token);
+      var loginName = response.data.number.NAME;
+      var authority =  response.data.number.AUTHORITY;
+      let name = `${loginName}_${authority}`;
+
+      window.location = `../signature/pages/dashboard.html?login=${name}`; 
+
+    } else if(response.data.message) {
+      errormessage="氏名コードまたはパスワード<br>の確認をしてください。";
+      document.getElementById("pass").value = "";
+    swallopen(errormessage);
+    };
+    console.log(response.data)
+  })
+
+  .catch((err) => {
+    errormessage="氏名コードまたはパスワード<br>の確認をしてください。";
+    document.getElementById("pass").value = "";
+    swallopen(errormessage);
+   });
+};
+
+
+
+
+function router2(){
+  axios.get(accessmainserver+'/clientes',{
+  headers:{
+    token:localStorage.getItem('token')
+  }
+}
+)
+}
+
+
+
+function swallopen(){
+  Swal.fire({
+  title: 'エラー',
+  icon: 'warning',
+  showCancelButton: true,
+  showConfirmButton: false,
+  cancelButtonText: '戻る',
+  width: 500,
+  html:`<span>${errormessage}</span>`,
+  customClass: "sweet-alert",
+  }).then((result) => {
+  if (result.value) {
+    Swal.fire({
+      icon: "success",
+      title: 'concluido',
+    }
+    )
+  }
+  });
+}
+
+
+
+    // manipula o sucesso da requisição
