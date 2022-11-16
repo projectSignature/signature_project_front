fdc7c97934f1f6ea2a1109aa745fb2c18650dac5
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-11-16 22:44:47 +0900

    Update index1.js

--- script/index1.js
+++ script/index1.js
@@ -973,8 +973,8 @@ function planget(){
 	     console.log(res)
 		const plan_existentes = []
 			 for(var i=0;i<res.length;i++){
-				 if(member_kubun==res[index].PLAN_KUBUN){
-					 console.log(res[index])
+				 if(member_kubun==res[i].PLAN_KUBUN){
+					 console.log(res[i])
 					// plan_existentes
 				 }
 	      // kakari_existentes.push(response.data.number[index].KAKARI_NAME)
