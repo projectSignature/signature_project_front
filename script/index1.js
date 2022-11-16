75989acb401dcc512fe484af8d3f021e7ff35fea
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-11-16 23:47:39 +0900

    Update index1.js

--- script/index1.js
+++ script/index1.js
@@ -977,7 +977,7 @@ function planget(){
 			 for(var i=0;i<res.length;i++){
 				 if(member_kubun==res[i].PLAN_KUBUN){
 					 console.log(res[i].PLAN_VALOR)
-			      document.getElementById(`plan-${plan_loop}-price`).innerHTML　= res[i].PLAN_VALOR
+			      document.getElementById(`plan-${plan_loop}-price`).innerHTML　= "\" + res[i].PLAN_VALOR
 						plan_loop = plan_loop +1 
 				 }
 	      // kakari_existentes.push(response.data.number[index].KAKARI_NAME)
