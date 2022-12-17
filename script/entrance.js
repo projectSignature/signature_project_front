8fa6572400b238fd90191bfcd960ee52b185e572
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-12-17 12:04:42 +0900

    Update entrance.js

--- script/entrance.js
+++ script/entrance.js
@@ -13,6 +13,10 @@ fetch('https://squid-app-ug7x6.ondigitalocean.app/calender/entrance')
     dadoCalender = res;
   })
 
+  //const japanStandardTime = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
+  //const japantime = new Date(japanStandardTime)
+  //console.log(japantime.getDay())
+
 //スライド移動
 function assignEventsInButtons() {
   document.getElementById('next').addEventListener('click', nextSlide)
@@ -24,23 +28,23 @@ function nextSlide() {
 function previousSlide() {
   cards.scrollLeft -= 1300
 }
-
+const mountedClasses = []
+var entrancearry = []
 function classesHandler(classes) {
-  console.log(classes)
-  const mountedClasses = []
+  entrancearry = classes
   for (let index = 0; index < classes.length; index++) {
     const currentClass = classes[index];
     if (currentClass.DESCRITION_1 != '') {
-        console.log(currentClass)
       let cardOfClass = mountCard(currentClass, index)
-      cardOfClass = cardOfClass + `<span class="nextClass ${(index >= classes.length - 1) ? "last" : ""}">&#8594;</span>`
-      // cards.innerHTML += cardOfClass;
-      mountedClasses.push(cardOfClass)
+      if(classes[index+1].DESCRITION_1 != ''&&index !=classes.length){
+        cardOfClass = cardOfClass + `<span class="nextClass ${(index >= classes.length - 1) ? "last" : ""}">&#8594;</span>`
+      }
+          mountedClasses.push(cardOfClass)
     };
   };
   const next = mountedClasses.filter(classMount => classMount.includes('content-coming-next'))
   const now = mountedClasses.filter(classMount => classMount.includes('now'))
-  const finished = mountedClasses.filter(classMount => classMount.includes('finish'))
+  const finished = mountedClasses.filter(classMount => classMount.includes('finished'))
   const order = [...now, ...next, ...finished]
   for(let i=0; i<order.length; i++){
     cards.innerHTML += order[i]
@@ -59,21 +63,20 @@ function mountCard(data, index) {
    const test = new Date('2022/11/27 15:40')
    if(currentTime>timeOfClass && currentTime<timeToFinish){
      status = "On going" //最中
-     console.log("in")
    }
 if(currentTime>timeToFinish && status != "On going"){
   let cardStruct = `
   <div class="card ${(currentTime.getHours() == timeOfClass.getHours()) ? 'now' : ""}">
       <span class="title">Finished</span>
-      <div class="content-finish" id="card${index}" onclick="entrance_regist()">
-          <span class="contentText-finished" name="desc1" >${data.DESCRITION_1}</span>
-          <span class="contentText-finished" name="desc2" >${data.DESCRITION_2}</span>
-          <span class="contentText-finished-time">finished at ${data.FINISH_TIME}</span>
+      <div class="content-finish" id="card${index}" onclick="entrance_regist(${index})">
+          <span class="contentText-finished" name="desc1" id="discrition${index}" >${data.DESCRITION_1}</span>
+          <span class="contentText-finished" name="desc2" id="subdiscrition${index}" >${data.DESCRITION_2}</span>
+          <span class="contentTimeText-coming-next" disabled="disable" id="starttime${index}" >Start at ${data.START_TIME}</span>
+          <span class="contentText-finished-time"   id="finishtime${index}">finished at ${data.FINISH_TIME}</span>
           </div>
   </div>`
   //cardStruct = cardStruct.replace(/{{image}}/g, ` <img src="${imgTeste}" />`);
-  localStorage.setItem('startTime', data.FINISH_TIME);
-
+//  localStorage.setItem('startTime', data.FINISH_TIME);
   return cardStruct;
 }else if(cards_count==0  && status != "On going"){
     let cardStruct = `
@@ -81,15 +84,15 @@ if(currentTime>timeToFinish && status != "On going"){
         <span class="title">
             ${(timeOfClass.getDate() < currentTime.getDate() || timeOfClass.getHours() < currentTime.getHours()) ? "Finished" : "Coming next"}
         </span>
-        <div class="content-coming-next" id="card${index}" onclick="entrance_regist()">
-            <span class="contentText-coming-next" name="desc1">${data.DESCRITION_1}</span>
-            <span class="contentText-coming-next" name="desc2">${data.DESCRITION_2}</span>
-            <span class="contentTimeText-coming-next">Start at ${data.START_TIME}</span>
-            <span class="contentTimeText-coming-next">Finish at ${data.FINISH_TIME}</span>
+        <div class="content-coming-next" id="card${index}" onclick="entrance_regist(${index})">
+            <span class="contentText-coming-next" name="desc1"  id="discrition${index}">${data.DESCRITION_1}</span>
+            <span class="contentText-coming-next" name="desc2"  id="subdiscrition${index}">${data.DESCRITION_2}</span>
+            <span class="contentTimeText-coming-next"   id="starttime${index}" >Start at ${data.START_TIME}</span>
+            <span class="contentTimeText-coming-next"  id="finishtime${index}">Finish at ${data.FINISH_TIME}</span>
             </div>
     </div>`
     cards_count = cards_count +1
-     localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
+  //   localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
     return cardStruct;
 }else if(status=="On going"){
   let cardStruct = `
@@ -97,14 +100,14 @@ if(currentTime>timeToFinish && status != "On going"){
       <span class="title">
           ${(timeOfClass.getDate() < currentTime.getDate() || timeOfClass.getHours() < currentTime.getHours()) ? "Finished" : "On going"}
       </span>
-      <div class="content-ongoing" id="card${index}" onclick="entrance_regist()">
-          <span class="contentText-ongoing" name="desc1" >${data.DESCRITION_1}</span>
-          <span class="contentText-ongoing" name="desc2" >${data.DESCRITION_2}</span>
-          <span class="contentTimeText-ongoing">Start at ${data.START_TIME}</span>
-          <span class="contentTimeText-ongoing">Finish at ${data.FINISH_TIME}</span>
+      <div class="content-ongoing" id="card${index}" onclick="entrance_regist(${index})">
+          <span class="contentText-ongoing" name="desc1" id="discrition${index}" >${data.DESCRITION_1}</span>
+          <span class="contentText-ongoing" name="desc2" id="subdiscrition${index}" >${data.DESCRITION_2}</span>
+          <span class="contentTimeText-ongoing"  id="starttime${index}" >Start at ${data.START_TIME}</span>
+          <span class="contentTimeText-ongoing"  id="finishtime${index}">Finish at ${data.FINISH_TIME}</span>
           </div>
   </div>`
-  localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
+//  localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
   cards_count = 0
   return cardStruct;
 }else{
@@ -113,20 +116,21 @@ if(currentTime>timeToFinish && status != "On going"){
       <span class="title">
           ${ (timeOfClass.getDate() < currentTime.getDate() || timeOfClass.getHours() < currentTime.getHours()) ? "Finished" : "Coming"}
       </span>
-      <div class="content-finish" id="card${index}" onclick="entrance_regist()">
-      <span class="contentText-finished" name="desc1" >${data.DESCRITION_1}</span>
-      <span class="contentText-finished" name="desc2" >${data.DESCRITION_2}</span>
-      <span class="contentText-finished-time">Start at ${data.START_TIME}</span>
-      <span class="contentText-finished-time">Finish at ${data.FINISH_TIME}</span>
+      <div class="content-finish" id="card${index}" onclick="entrance_regist(${index})">
+      <span class="contentText-finished" name="desc1" id="discrition${index}" >${data.DESCRITION_1}</span>
+      <span class="contentText-finished" name="desc2" id="subdiscrition${index}" >${data.DESCRITION_2}</span>
+      <span class="contentText-finished-time"  id="starttime${index}" >Start at ${data.START_TIME}</span>
+      <span class="contentText-finished-time"  id="finishtime${index}" >Finish at ${data.FINISH_TIME}</span>
           </div>
   </div>`
-  
-  localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
-  
+
+///  localStorage.setItem('startTime', [data.START_TIME, data.FINISH_TIME]);
+
 //  cardStruct = cardStruct.replace(/{{image}}/g, ` <img src="${imgTeste}" />`);
 //{{image}}←div の中に入れる
   return cardStruct;
 }
+
 };
 
 function getFormattedTime(time) {
@@ -136,46 +140,11 @@ function getFormattedTime(time) {
 assignEventsInButtons()
 //classesHandler()
 
-function input_regist1() {
+function input_regist(data) {
   input_length_count()
-  document.getElementById('keys_entrance').value += 1
+  document.getElementById('keys_entrance').value += data
 }
-function input_regist2() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 2
-}
-function input_regist3() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 3
-}
-function input_regist4() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 4
-}
-function input_regist5() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 5
-}
-function input_regist6() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 6
-}
-function input_regist7() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 7
-}
-function input_regist8() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 8
-}
-function input_regist9() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 9
-}
-function input_regist0() {
-  input_length_count()
-  document.getElementById('keys_entrance').value += 0
-}
+
 function input_clear() {
   document.getElementById('keys_entrance').value = ""
 }
@@ -185,15 +154,12 @@ function input_back() {
 
 function input_length_count() {
   var str = document.getElementById('keys_entrance').value
-  console.log(str.length)
   if (str.length == 4) {
     errormessage = 'パスワードは4桁で入力してください'
     swallerror(errormessage)
   }
 
 }
-
-
 
 function swallerror(errormessage) {
   Swal.fire({
@@ -214,8 +180,7 @@ function swallerror(errormessage) {
 
 //tratamento do registro de aula
 
-
-function entrance_regist() {
+function entrance_regist(number) {
   Swal.fire({
     showCancelButton: false,
     showConfirmButton: true,
@@ -226,28 +191,27 @@ function entrance_regist() {
       <input type="password" id="keys_entrance" hiden/>
       <div id="main-div">
        <div id="first-row" class="swall-row">
-         <div id="en1" class="number-div" onclick="input_regist1()">1</div>
-         <div id="en2" class="number-div" onclick="input_regist2()">2</div>
-         <div id="en3" class="number-div" onclick="input_regist3()">3</div>
+         <div id="en1" class="number-div" onclick="input_regist(1)">1</div>
+         <div id="en2" class="number-div" onclick="input_regist(2)">2</div>
+         <div id="en3" class="number-div" onclick="input_regist(3)">3</div>
        </div>
        <div id="second-row" class="swall-row">
-         <div id="en4" class="number-div" onclick="input_regist4()">4</div>
-         <div id="en5" class="number-div" onclick="input_regist5()">5</div>
-         <div id="en6" class="number-div" onclick="input_regist6()">6</div>
+         <div id="en4" class="number-div" onclick="input_regist(4)">4</div>
+         <div id="en5" class="number-div" onclick="input_regist(5)">5</div>
+         <div id="en6" class="number-div" onclick="input_regist(6)">6</div>
        </div>
        <div id="third-row" class="swall-row">
-         <div id="en7" class="number-div" onclick="input_regist7()">7</div>
-         <div id="en8" class="number-div" onclick="input_regist8()">8</div>
-         <div id="en9" class="number-div" onclick="input_regist9()">9</div>
+         <div id="en7" class="number-div" onclick="input_regist(7)">7</div>
+         <div id="en8" class="number-div" onclick="input_regist(8)">8</div>
+         <div id="en9" class="number-div" onclick="input_regist(9)">9</div>
        </div>
        <div id="fourth-row" class="swall-row">
-         <div id="en0" class="number-div" onclick="input_regist0()">0</div>
+         <div id="en0" class="number-div" onclick="input_regist(0)">0</div>
          <div id="clear" class="number-div" onclick="input_clear()">Clear</div>
          <div id="en11" class="number-div" onclick="input_back()">←</div>
        </di>
       </div>
           <style>
-
           #main-div{
             width:95%;
           }
@@ -273,6 +237,7 @@ function entrance_regist() {
             height:10vh;
               width:90%;
             border-radius: 10px;
+            font-size:3vw;
           }
           #en1{
       margin-left:8%;
@@ -300,7 +265,6 @@ function entrance_regist() {
       })
         .then((x) => x.json())
         .then((res) => {
-
           Swal.fire({
             title: 'Registrando!',
             didOpen: () => { Swal.showLoading() }
@@ -309,19 +273,25 @@ function entrance_regist() {
           if (res[0]) {
             localStorage.setItem('id', res[0].id);
             localStorage.setItem('GYM_ID', res[0].GYM_ID);
-            Swal.fire({
-              icon: "success",
-              title: 'concluido!',
-              didOpen: () => { Swal.hideLoading() }
-            }).then((result) => {
-              if (result.isConfirmed) {
-                addClient_again()
-              };
-            });
+            addClient_again(number)
+            if(entrancearry[number].GRADUATION_FLUG==1){
+              entrance_count(res[0].id)
+            }else{
+
+            }
+            //Swal.fire({
+            //  icon: "success",
+            //  title: 'concluido!',
+            //  didOpen: () => { Swal.hideLoading() }
+          //  }).then((result) => {
+            //  if (result.isConfirmed) {
+              //  addClient_again()
+              //};
+            //});
           } else {
             Swal.fire({
               icon: "error",
-              title: 'Algo errado!',
+              title: 'password invalid!',
               didOpen: () => { Swal.hideLoading() }
             });
           }
@@ -331,52 +301,46 @@ function entrance_regist() {
   });
 };
 
-getTarget()
 
-function getTarget() {
-  const cardClicked = document.querySelector('#cards');
-  cardClicked.addEventListener('click', (e) => {
+function entrance_count(data){
+  fetch("https://squid-app-ug7x6.ondigitalocean.app/graduationlist", {
+    method: "POST",
+    body: JSON.stringify({ opt1: "", opt2: "" }),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((x) => x.json())
+    .then((res) => {
+      clients1 = res;
+      for (let index = 0; index < clients1.length; index++) {
+        if(clients1[index].nm_member_id==data){
+          let obj = {
+            id: clients1[index].nm_member_id,
+            lesson_after: (clients1[index].lesson_after -0) + 1
+          };
+          const attr = {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+          };
+          fetch("https://squid-app-ug7x6.ondigitalocean.app/graduationafter", attr)
+            .then((x) => x.json())
+            .then((response) => {
+            });
+        }
+      }
+    ;
+    });
+}
 
-    switch (e.target.id) {
-      case 'card0':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        console.log("este", e.target.childNodes[1].innerText);
-        console.log("este", e.target.childNodes[3].innerText);
-        break;
-      case 'card1':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        break;
-      case 'card2':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        break;
-      case 'card3':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        break;
-      case 'card4':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        break;
-      case 'card5':
-        localStorage.setItem('decricoes', [e.target.childNodes[1].innerText, e.target.childNodes[3].innerText]);
-        break;
-      default:
-        console.log("error...")
-    }
-
-  });
-};
-
-/*function seletorTarget(event, numberOfTarget) {
-  return event.target.childNodes[numberOfTarget].innerText
-};*/
-
-function addClient_again() {
+function addClient_again(number) {
+  var starttime = document.getElementById(`starttime${number}`).innerHTML.substr(10,5)
+  var finishtime = document.getElementById(`finishtime${number}`).innerHTML.substr(10,5)
   var obj = {
-    LESSON_NAME: localStorage.getItem('decricoes').split(',').toString().replace(",","~"),
-    LESSON_HOUR: localStorage.getItem('startTime').split(',').toString().replace(",","~"),
+    LESSON_NAME: document.getElementById(`discrition${number}`).innerHTML + "_" + document.getElementById(`subdiscrition${number}`).innerHTML,
+    LESSON_HOUR: starttime +"~"+finishtime,
     MEMBER_ID: localStorage.getItem('id'),
     GYM_ID: localStorage.getItem('GYM_ID')
   }
-
   fetch('https://squid-app-ug7x6.ondigitalocean.app/registerentrance', {
     method: 'POST',
     body: JSON.stringify(obj),
@@ -384,6 +348,11 @@ function addClient_again() {
   })
     .then((x) => x.json())
     .then((res) => {
-      console.log(res)
+      Swal.fire({
+        icon: "success",
+        title: 'concluido!',
+        timer: '1000',
+      }).then((result) => {
+      });
     })
 };
