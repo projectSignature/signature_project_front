791e280871964e09adf3ab3c1f42551b4749ed7d
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-12-22 21:06:30 -0300

    Update entrance.js

--- script/entrance.js
+++ script/entrance.js
@@ -323,6 +323,9 @@ function entrance_count(data){
             .then((x) => x.json())
             .then((response) => {
               //acrescentar aqui o email , caso a aula for numero de
+              fetch(`https://squid-app-ug7x6.ondigitalocean.app/lesson_after/${attr.id}`)
+              .then((x) => x.json())
+              .then((res) => {console.log(res)})
             });
         }
       }
