7c42532030792ee0371273fdecc1d591a89119a9
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-01-23 22:17:21 +0900

    Update entrance.js

--- script/entrance.js
+++ script/entrance.js
@@ -306,14 +306,11 @@ function entrance_count(data){
     ;
     });
 }
-const date = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
-const japanweekeday = date.getDay()
-var d1 = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' +('0' + date.getDate()).slice(-2) + ' ' +  ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2) + '.' + date.getMilliseconds();
 
 function addEntrance(number) {
   var starttime = clientes[number].START_TIME
   var finishtime = clientes[number].FINISH_TIME
-  const date = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+  const date = new Date()
   const japanweekeday = date.getDay()
   var d1 = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' +('0' + date.getDate()).slice(-2) + ' ' +  ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2) + '.' + date.getMilliseconds();
   var obj = {
