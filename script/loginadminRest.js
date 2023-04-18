a335d54eba356f9be75c6bbda250fad49fbe0e25
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-04-18 14:13:40 -0300

    Add files via upload

---
+++ script/loginadminRest.js
@@ -0,0 +1,72 @@
+
+let accessmainserver = 'https://squid-app-ug7x6.ondigitalocean.app'　　//メインサーバーのチェックアクセス先
+let user
+let password
+let errormessage
+document.getElementById("login-bottom").addEventListener("click", login_check)//ログインボタンクリック時の操作
+//ログイン情報の確認をする処理、IDが空白かどうか、その後PASSがくうはくかどうか、TRUEの場合Swal処理
+function login_check(user, password) {
+  console.log('in')
+  user = document.getElementById("user").value;　　　　//ユーザー名
+  password = document.getElementById("pass").value;　 //パスワード
+
+  if (user == "") {
+    errormessage = "Enter your username"
+    swallopen(errormessage)
+  } else {
+    if (password == "") {
+      errormessage = "Enter your password"
+      swallopen(errormessage)
+    } else {
+      login_request(user, password) //chamar função de login passando login e senha
+    }
+  }
+}
+//validar dados
+async function login_request(user, password) {
+  console.log('in')
+  console.log(user)
+  console.log(password)
+  await axios.post(accessmainserver + '/authRestmember', {
+    numbers: user,
+    password: password
+  })
+    .then((response) => {
+      console.log(response)
+     if (response.status==200) {
+       console.log('in')
+       sessionStorage.setItem("name",response.data[0].worker_name)
+       sessionStorage.setItem("id",response.data[0].id)
+       sessionStorage.setItem("restid",response.data[0].rest_id)
+       window.location = `../pages/dashboardrest.html`;
+     }else{
+       console.log('no')
+     }
+    })
+    .catch((err) => {
+      errormessage = "Check username and password";
+      //document.getElementById("pass").value = "";
+    //  swallopen(errormessage);
+    });
+};
+
+function swallopen() {
+  Swal.fire({
+    title: 'Error',
+    icon: 'warning',
+    showCancelButton: true,
+    showConfirmButton: false,
+    cancelButtonText: 'back',
+    width: 500,
+    html: `<span>${errormessage}</span>`,
+    customClass: "sweet-alert",
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        icon: "success",
+        title: 'concluido',
+      }
+      )
+    }
+  });
+}
